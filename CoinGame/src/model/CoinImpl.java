4baf1dd0b4965ef03a544539e96a0af6daaf1205
Lockie Boughton <officiallockie@gmail.com>
2019-08-07 14:25:59 +0200

    Created all necessary classes
    Created classes: CoinImpl, CoinPairImpl, GameEngineImpl, and SimplePlayer. All implementing their respected interfaces.

---
+++ CoinGame/src/model/CoinImpl.java
@@ -0,0 +1,10 @@
+package model;
+
+//Created by Lockie
+
+import model.interfaces.Coin;
+
+public class CoinImpl implements Coin
+{
+
+}
---
+++ CoinGame/src/model/GameEngineImpl.java
@@ -0,0 +1,10 @@
+package model;
+
+//Created by Lockie
+
+import model.interfaces.GameEngine;
+
+public class GameEngineImpl implements GameEngine
+{
+
+}

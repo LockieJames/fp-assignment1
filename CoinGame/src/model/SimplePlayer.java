aca0ef8ea656619fc03039bf8080e818dac9fbc7
Lockie Boughton <officiallockie@gmail.com>
2019-08-08 12:39:50 +0200

    Created SimplePlayer method headers
    getPlayerName, setPlayerName, getPoints, setPoints, getPlayerId, setBet, getBet, setBetType, getBetType, resetBet, getResult, setResult

--- CoinGame/src/model/SimplePlayer.java
+++ CoinGame/src/model/SimplePlayer.java
@@ -2,9 +2,89 @@ package model;
 
 //Created by Lockie
 
+import model.enumeration.BetType;
+import model.interfaces.CoinPair;
 import model.interfaces.Player;
 
 public class SimplePlayer implements Player
 {
+    private String playerId;
+    private String playerName;
+    private int initialPoints;
 
+    //??
+    private int points;
+
+    public SimplePlayer(String playerId, String playerName, int initialPoints)
+    {
+        this.playerId = playerId;
+        this.playerName = playerName;
+        this.initialPoints = initialPoints;
+    }
+
+    public String getPlayerName()
+    {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName)
+    {
+        this.playerName = playerName;
+    }
+
+    public int getPoints()
+    {
+        return points;
+    }
+
+    public void setPoints(int points)
+    {
+        this.points = points;
+    }
+
+    public String getPlayerId()
+    {
+        return playerId;
+    }
+
+    public boolean setBet(int bet)
+    {
+
+    }
+
+    public int getBet()
+    {
+
+    }
+
+    public void setBetType(BetType betType)
+    {
+
+    }
+
+    public BetType getBetType()
+    {
+
+    }
+
+    public void resetBet()
+    {
+
+    }
+
+    public CoinPair getResult()
+    {
+
+    }
+
+    public void setResult(CoinPair coinPair)
+    {
+
+    }
+
+    @Override
+    public String toString()
+    {
+
+    }
 }
